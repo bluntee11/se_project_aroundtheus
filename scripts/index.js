c9f65d3c3f8ff26d72235a04bcce450a9c1d8770
bluntee11 <luntebrit@gmail.com>
2024-08-14 22:24:37 -0600

    Phase 4
    Phase 4 of project 4
    -made modal able to accept new text and have text that is currently set in profile in box when opened
    -made cards render through java and figured out how to make sure css targeted cards via java

--- scripts/index.js
+++ scripts/index.js
@@ -30,11 +30,80 @@ const initialCards = [
 const profileEditButton = document.querySelector("#profile-edit-button");
 const profileEditModal = document.querySelector("#profile-edit-modal");
 const closeButton = document.querySelector("#profile-modal-close");
+const profileName = document.querySelector(".profile__name");
+const profileDescription = document.querySelector(".profile__description");
+const profileNameInput = document.querySelector("#form-input-name");
+const profileDescriptionInput = document.querySelector(
+  "#form-input-description"
+);
+const profileEditForm = profileEditModal.querySelector(".modal__form");
+const cardListElement = document.querySelector(".cards__list");
+const cardTemplate =
+  document.querySelector("#card-template").content.firstElementChild;
+
+/*Functions*/
+
+function closePopup() {
+  profileEditModal.classList.remove("modal_opened");
+}
+
+function getCardElement(cardData) {
+  /*clone the template element with all its content and store it in a cardElement variable*/
+  const cardElement = cardTemplate.cloneNode(true);
+
+  /*access the card title and image and store them in variables*/
+  const cardImageElement = cardElement.querySelector(".card__image");
+  const cardTitleElement = cardElement.querySelector(".card__title");
+
+  /*set the path to the image to the link field of the object*/
+  cardImageElement.src = cardData.link;
+
+  /*set the image alt text to the name field of the object*/
+  cardImageElement.alt = cardData.name;
+
+  /*set the card title to the name field of the object, too*/
+  cardTitleElement.textContent = cardData.name;
+
+  /*return the ready HTML element with the filled-in data*/
+  return cardElement;
+}
+
+/*Handlers*/
+
+function handleProfileEditSubmit(event) {
+  event.preventDefault();
+  profileName.textContent = profileNameInput.value;
+  profileDescription.textContent = profileDescriptionInput.value;
+  closePopup();
+}
+
+/*Event Listeners*/
 
 profileEditButton.addEventListener("click", () => {
   profileEditModal.classList.add("modal_opened");
+  profileNameInput.value = profileName.textContent;
+  profileDescriptionInput.value = profileDescription.textContent;
 });
 
-closeButton.addEventListener("click", () => {
-  profileEditModal.classList.remove("modal_opened");
+closeButton.addEventListener("click", closePopup);
+
+profileEditForm.addEventListener("submit", handleProfileEditSubmit);
+
+initialCards.forEach((cardData) => {
+  const cardElement = getCardElement(cardData);
+  cardListElement.append(cardElement);
+});
+
+console.log({
+  profileEditButton,
+  profileEditModal,
+  closeButton,
+  profileEditForm,
+  profileName,
+  profileDescription,
+  profileNameInput,
+  profileDescriptionInput,
+  cardListElement,
+  initialCards,
+  cardTemplate,
 });
